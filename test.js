03e6c0f3a58395c4e9e1d7ce81c9cdd4e5aaa09a
sergiouceda <uceda.sergio@gmail.com>
2023-08-12 00:44:19 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-var araHidden = document.querySelector('div.ara-body div[style="display:none"]');
+try {
+  var araHidden = document.querySelector('div.ara-body div[style="display:none"]');
+  araHidden.style.display = 'unset';
+  araHidden.setAttribute("class","ara-body");
+} catch(e){}
 
-araHidden.style.display = 'unset'; 
-
-araHidden.setAttribute("class","ara-body");
+try {
+  document.querySelector("div.gsc-adBlock").remove();
+} catch(e){}
