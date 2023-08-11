2af84256f9ba2b05698df8b4f4a14797543c06dd
sergiouceda <uceda.sergio@gmail.com>
2023-08-12 00:51:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,13 @@ try {
   var araHidden = document.querySelector('div.ara-body div[style="display:none"]');
   araHidden.style.display = 'unset';
   araHidden.setAttribute("class","ara-body");
-} catch(e){}
+}catch(e){}
 
 try {
   document.querySelector("div.gsc-adBlock").remove();
-} catch(e){}
+}catch(e){}
+
+try {
+  document.querySelectorAll("*[class*='ezoic']").forEach(e => e.remove());
+  document.querySelector("body").style.fontSize ="16px";
+}catch(e){}
