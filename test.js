09380a3edb2185ae185c0b70130c3c1984cf754b
sergiouceda <uceda.sergio@gmail.com>
2023-09-27 12:21:49 +0200

    Update test.js

--- test.js
+++ test.js
@@ -8,4 +8,15 @@ try {
   document.querySelectorAll("*[class*='gsc-adBlock']").forEach(e => e.remove());
 }catch(e){}
 
+if(window.location.host == "cse.google.com"){
+       
+        function removeTarget(){
+                document.querySelectorAll('a.gs-title').forEach(a => a.setAttribute('target',"_self"));
+        }
+       
+        setInterval(removeTarget, 500);        
+
+}
+
+
 
